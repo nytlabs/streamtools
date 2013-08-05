746df3b82ea9621962a2d8683e1818308f8b78ea
Agnes Chang <kradeki@gmail.com>
2013-08-05 15:01:00 -0400

    added comments to clarify what import/random_stream does at the moment

--- import/random_stream/random_stream.go
+++ import/random_stream/random_stream.go
@@ -1,58 +1,61 @@
+// Fakes a stream of data at your convenience. Doesn't yet quite generate random stream.
+// Takes a string as input and puts a random-time-offset message on an nsq.
+
 package main
 
 import (
-    "flag"
-    "github.com/bitly/go-simplejson"
-    "time"
-    "math/rand"
-    "io/ioutil"
-    //"strconv"
-    "log"
-    "net/http"
-    "bytes"
+	"flag"
+	"github.com/bitly/go-simplejson"
+	"io/ioutil"
+	"math/rand"
+	"time"
+	//"strconv"
+	"bytes"
+	"log"
+	"net/http"
 )
 
 var (
-    topic            = flag.String("topic", "test", "nsq topic")
-    nsqHTTPAddrs     = flag.String("nsqd-tcp-address", "127.0.0.1:4151", "nsqd TCP address")
-    jsonMsgPath      = flag.String("file","test.json","json file to send")
-    timeKey          = flag.String("key","t","key that holds time")
+	topic        = flag.String("topic", "test", "nsq topic")
+	nsqHTTPAddrs = flag.String("nsqd-tcp-address", "127.0.0.1:4151", "nsqd TCP address")
+	jsonMsgPath  = flag.String("file", "test.json", "json file to send")
+	timeKey      = flag.String("key", "t", "key that holds time")
 )
 
-func writer(msgText []byte){
-    msgJson, _ := simplejson.NewJson(msgText) 
-    client := &http.Client{}
+func writer(msgText []byte) {
+	msgJson, _ := simplejson.NewJson(msgText)
+	client := &http.Client{}
 
-    c := time.Tick( 5 * time.Second)
-    r := rand.New(rand.NewSource(99))
+	c := time.Tick(5 * time.Second)
+	r := rand.New(rand.NewSource(99))
 
-    for now := range c {
-        a := int64( r.Float64() * 10000000000 )
-        strTime := now.UnixNano() - a
-        msgJson.Set(*timeKey, int64(strTime / 1000000) )
-        outMsg, _ := msgJson.Encode() 
-        msgReader := bytes.NewReader(outMsg)
-        resp, err := client.Post("http://" + *nsqHTTPAddrs + "/put?topic=" + *topic,"data/multi-part", msgReader)
-        if err != nil {
-            log.Fatalf(err.Error())
-        }
-        resp.Body.Close()
-    }
+	for now := range c {
+		a := int64(r.Float64() * 10000000000)
+		strTime := now.UnixNano() - a
+		msgJson.Set(*timeKey, int64(strTime/1000000))
+		outMsg, _ := msgJson.Encode()
+		msgReader := bytes.NewReader(outMsg)
+		resp, err := client.Post("http://"+*nsqHTTPAddrs+"/put?topic="+*topic, "data/multi-part", msgReader)
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		resp.Body.Close()
+	}
 }
 
-func main(){
-    
-    flag.Parse()
+func main() {
 
-    stopChan := make(chan int)
+	flag.Parse()
 
-    content, err := ioutil.ReadFile(*jsonMsgPath)
+	stopChan := make(chan int)
 
-    if err != nil {
-        log.Fatal(err.Error())
-    }
+	content, err := ioutil.ReadFile(*jsonMsgPath)
 
-    go writer(content)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
-    <- stopChan
+	go writer(content)
+
+	<-stopChan
 }
