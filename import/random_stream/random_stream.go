0fb4d9b9b72ab8d45abf741366f90aacd3d6e3b9
Agnes Chang <kradeki@gmail.com>
2013-08-06 16:08:14 -0400

    updated 'import/random_stream' to generate variable length utf8 string and signed/unsigned floats under nested key

--- import/random_stream/random_stream.go
+++ import/random_stream/random_stream.go
@@ -1,17 +1,19 @@
 // Fakes a stream of data at your convenience.
-// This contains a random time stamp, and random length array of random integers
+// This contains a random time stamp, a random length array of random integers,
+// and a nested json blob which in turn contains a random length utf8 string, an
+// unsigned float, and a signed float.
 
 package main
 
 import (
+	"bytes"
 	"flag"
 	"github.com/bitly/go-simplejson"
-	"math/rand"
-	"time"
-	//"strconv"
-	"bytes"
 	"log"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
 )
 
 var (
@@ -21,6 +23,9 @@ var (
 
 	nsqHTTPAddrs = "127.0.0.1:4151"
 )
+
+var raw_string = "∰ ∈ ∉ ∌ ∂ According to ISO 10646-1:2000, sections D.7 and 2.3c, a device receiving UTF-8 shall interpret a \"malformed sequence in the same way that it interprets a character &amp;that is outside тіршілік-тынысының қолайлы дамыту, қауіпсіздігін, the adopted subset\" and “characters that are not within the adopted 남한산성(南漢山城)은 광주시, κόσμε 성남시, 산성으로, 784-16에 속해있다. subset shall be indicated to the user” by a receiving device. It can cost just $125 to buy a package of bees, and there&rsquo; is no real maintenance involved. Bees are typically bred in the south and shipped north in April, sent to beekeepers in a cage the size of a lunch box that can be mailed through the United States Postal Service. To buy a mature hive that is already producing honey, like the ones the Durst Organization has, can cost $300 to as high as $1,000. “People look at pogosto ne da razlikovati od nestrupenih vrst ter da žrtev na začetku nemalokrat zgleda neprizadeto, je treba v primeru ugriza takoj poiskati zdravniško pomoč moms and say, ‘oh they’re just low income and that’s it,'” she said. “I think parents have changed. विकिपीडिया  यह यथासम्भव निष्पक्ष दृष्टिकोण वाली सूOur income may still be low, but we’re more educated”"
+var string_bank = strings.Fields(raw_string)
 
 func writer() {
 	msgJson, _ := simplejson.NewJson([]byte("{}"))
@@ -37,12 +42,21 @@ func writer() {
 		msgJson.Set("a", 10)
 
 		b := make([]int, rand.Intn(10))
-
 		for i, _ := range b {
 			b[i] = rand.Intn(100)
 		}
-
 		msgJson.Set("b", b)
+
+		nestJson, _ := simplejson.NewJson([]byte("{}"))
+		l := rand.Intn(20) + 10
+		d := make([]string, l)
+		for i, _ := range d {
+			d[i] = string_bank[rand.Intn(len(string_bank))]
+		}
+		nestJson.Set("d", strings.Join(d, " ")+".")
+		nestJson.Set("e", rand.Float32()*8888)
+		nestJson.Set("f", rand.Float32()-rand.Float32()*32)
+		msgJson.Set("c", nestJson)
 
 		outMsg, _ := msgJson.Encode()
 		msgReader := bytes.NewReader(outMsg)
