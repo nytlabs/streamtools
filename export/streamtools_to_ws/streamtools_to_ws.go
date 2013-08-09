f04d42527af34b354728e3ce9d74e512dae34dd9
Ubuntu <nikhan@gmail.com>
2013-08-09 00:25:56 +0000

    gofmt

--- export/streamtools_to_ws/streamtools_to_ws.go
+++ export/streamtools_to_ws/streamtools_to_ws.go
@@ -1,122 +1,122 @@
 package main
 
 import (
-    "flag"
-    "log"
-    "github.com/bitly/nsq/nsq"
-    "code.google.com/p/go.net/websocket"
-    "net/http"
+	"code.google.com/p/go.net/websocket"
+	"flag"
+	"github.com/bitly/nsq/nsq"
+	"log"
+	"net/http"
 )
 
 var (
-    inTopic          = flag.String("in_topic", "", "topic to read from")
-    channelName      = "ws_" + *inTopic
-    lookupdHTTPAddrs = "127.0.0.1:4161"
-    httpAddress      = flag.String("http-address", "0.0.0.0:8080", "<addr>:<port> to listen on for HTTP clients")
-    maxInFlight      = flag.Int("max-in-flight", 100, "max number of messages to allow in flight")
+	inTopic          = flag.String("in_topic", "", "topic to read from")
+	channelName      = "ws_" + *inTopic
+	lookupdHTTPAddrs = "127.0.0.1:4161"
+	httpAddress      = flag.String("http-address", "0.0.0.0:8080", "<addr>:<port> to listen on for HTTP clients")
+	maxInFlight      = flag.Int("max-in-flight", 100, "max number of messages to allow in flight")
 )
 
-type SyncHandler struct {}
+type SyncHandler struct{}
 
 func (self *SyncHandler) HandleMessage(m *nsq.Message) error {
-    h.broadcast <- string(m.Body)
-    return nil
+	h.broadcast <- string(m.Body)
+	return nil
 }
 
 // https://gist.github.com/garyburd/1316852
 type connection struct {
-    ws *websocket.Conn
-    send chan string
+	ws   *websocket.Conn
+	send chan string
 }
- 
+
 func (c *connection) reader() {
-    for {
-        var message string
-        err := websocket.Message.Receive(c.ws, &message)
-        if err != nil {
-            break
-        }
-    }
-    c.ws.Close()
+	for {
+		var message string
+		err := websocket.Message.Receive(c.ws, &message)
+		if err != nil {
+			break
+		}
+	}
+	c.ws.Close()
 }
- 
+
 func (c *connection) writer() {
-    for message := range c.send {
-        err := websocket.Message.Send(c.ws, message)
-        if err != nil {
-            break
-        }
-    }
-    c.ws.Close()
+	for message := range c.send {
+		err := websocket.Message.Send(c.ws, message)
+		if err != nil {
+			break
+		}
+	}
+	c.ws.Close()
 }
- 
+
 func wsHandler(ws *websocket.Conn) {
-    c := &connection{send: make(chan string, 256), ws: ws}
-    h.register <- c
-    defer func() { h.unregister <- c }()
-    go c.writer()
-    c.reader()
+	c := &connection{send: make(chan string, 256), ws: ws}
+	h.register <- c
+	defer func() { h.unregister <- c }()
+	go c.writer()
+	c.reader()
 }
 
 type hub struct {
-    connections map[*connection]bool
-    broadcast chan string
-    register chan *connection
-    unregister chan *connection
+	connections map[*connection]bool
+	broadcast   chan string
+	register    chan *connection
+	unregister  chan *connection
 }
- 
+
 var h = hub{
-    broadcast:   make(chan string),
-    register:    make(chan *connection),
-    unregister:  make(chan *connection),
-    connections: make(map[*connection]bool),
+	broadcast:   make(chan string),
+	register:    make(chan *connection),
+	unregister:  make(chan *connection),
+	connections: make(map[*connection]bool),
 }
- 
+
 func (h *hub) run() {
-    for {
-        select {
-        case c := <-h.register:
-            h.connections[c] = true
-        case c := <-h.unregister:
-            delete(h.connections, c)
-            close(c.send)
-        case m := <-h.broadcast:
-            for c := range h.connections {
-                select {
-                case c.send <- m:
-                default:
-                    delete(h.connections, c)
-                    close(c.send)
-                    go c.ws.Close()
-                }
-            }
-        }
-    }
+	for {
+		select {
+		case c := <-h.register:
+			h.connections[c] = true
+		case c := <-h.unregister:
+			delete(h.connections, c)
+			close(c.send)
+		case m := <-h.broadcast:
+			for c := range h.connections {
+				select {
+				case c.send <- m:
+				default:
+					delete(h.connections, c)
+					close(c.send)
+					go c.ws.Close()
+				}
+			}
+		}
+	}
 }
 
-func main(){
-    flag.Parse()
+func main() {
+	flag.Parse()
 
-    go h.run()
+	go h.run()
 
-    r, err := nsq.NewReader(*inTopic, channelName)
-    if err != nil{
-        log.Fatal("could not make reader")
-    }
+	r, err := nsq.NewReader(*inTopic, channelName)
+	if err != nil {
+		log.Fatal("could not make reader")
+	}
 
-    r.SetMaxInFlight(*maxInFlight)
-    r.AddHandler(&SyncHandler{})
-    
-    err = r.ConnectToLookupd(lookupdHTTPAddrs)
-    if err != nil{
-        log.Fatal("could not connect to nsq")
-    }
+	r.SetMaxInFlight(*maxInFlight)
+	r.AddHandler(&SyncHandler{})
 
-    http.Handle("/ws", websocket.Handler(wsHandler))
+	err = r.ConnectToLookupd(lookupdHTTPAddrs)
+	if err != nil {
+		log.Fatal("could not connect to nsq")
+	}
 
-    if err = http.ListenAndServe(*httpAddress, nil); err != nil {
-        log.Fatal("ListenAndServe:", err)
-    }
+	http.Handle("/ws", websocket.Handler(wsHandler))
 
-    <- r.ExitChan
+	if err = http.ListenAndServe(*httpAddress, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+
+	<-r.ExitChan
 }
