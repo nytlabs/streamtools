bb8588827f4f870164b1b4578cb12e6dcb0ba9b9
Ubuntu <nikhan@gmail.com>
2013-09-26 14:20:26 +0000

    adding mask

---
+++ blocks/mask/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "flag"
+    "github.com/bitly/go-simplejson"
+    "github.com/nytlabs/stream_tools/streamtools"
+    "log"
+)
+
+var (
+    readTopic  = flag.String("read_topic", "", "topic to write to")
+    writeTopic = flag.String("write_topic", "", "topic to write to")
+    command    = flag.String("mask", "", "mask JSON")
+)
+
+func main() {
+    log.SetFlags(log.Lshortfile)
+    flag.Parse()
+
+    log.Println("reading from", *readTopic)
+    log.Println("writing to", *writeTopic)
+    log.Println("mask ", *command)
+
+    block := streamtools.NewTransferBlock(streamtools.Mask, "mask")
+    rule, err := simplejson.NewJson([]byte(*command))
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+    block.RuleChan <- rule
+    block.Run(*readTopic, *writeTopic, "8080")
+}
