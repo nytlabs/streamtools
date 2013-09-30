f00149ad25a51a330674b10904ee9b7365419ad5
Mike Dewar <mikedewar@gmail.com>
2013-09-30 19:28:25 +0000

    logging ideas

--- blocks/random_in/main.go
+++ blocks/random_in/main.go
@@ -3,14 +3,25 @@ package main
 import (
 	"flag"
 	"github.com/nytlabs/streamtools/streamtools"
+	"log"
+	"os"
 )
 
 var (
 	topic = flag.String("topic", "", "topic to write to")
+	name  = flag.String("name", "random_in", "name of block")
 )
 
 func main() {
 	flag.Parse()
-	random := streamtools.NewOutBlock(streamtools.Random, "random_stream")
+
+	log.SetPrefix(" [" + *name + "] ")
+	logfile, err := os.Create(*name + ".log")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	log.SetOutput(logfile)
+
+	random := streamtools.NewOutBlock(streamtools.Random, *name)
 	random.Run(*topic, "8081")
 }
