c04c60fd818e46ed47be810ab5d2c006ca878ca2
Mike Dewar <mikedewar@gmail.com>
2013-09-30 21:13:38 +0000

    missed a ibt

--- blocks/date_in/main.go
+++ blocks/date_in/main.go
@@ -10,23 +10,18 @@ import (
 var (
 	topic     = flag.String("topic", "", "topic to write to")
 	fmtString = flag.String("format", "", "format string (use Mon Jan 2 15:04:05 -0700 MST 2006)")
-	name      = flag.String("name", "date_in", "name of block")
+	name      = flag.String("name", "date-in", "name of block")
 )
 
 func main() {
 	flag.Parse()
 	streamtools.SetupLogger(name)
-
-	block := streamtools.NewOutBlock(streamtools.Date, "date_stream")
-
+	block := streamtools.NewOutBlock(streamtools.Date, *name)
 	rule, err := simplejson.NewJson([]byte("{}"))
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-
 	rule.Set("fmtString", *fmtString)
 	block.RuleChan <- rule
-
 	block.Run(*topic, "8081")
-
 }
