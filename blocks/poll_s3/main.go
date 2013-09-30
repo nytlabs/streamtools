5828d9ee1f20cd3aaf1fa33f14e1b47188bc48fb
Mike Dewar <mikedewar@gmail.com>
2013-09-30 21:27:28 +0000

    found a nother bit

--- blocks/poll_s3/main.go
+++ blocks/poll_s3/main.go
@@ -8,12 +8,12 @@ import (
 )
 
 var (
-	pollInterval = flag.Float64("poll_interval", 300, "check S3 every X seconds")
-	writeTopic   = flag.String("write_topic", "", "streamtools topic to write to")
+	pollInterval = flag.Float64("poll-interval", 300, "check S3 every X seconds")
+	writeTopic   = flag.String("write-topic", "", "streamtools topic to write to")
 	bucket       = flag.String("bucket", "", "s3 bucket")
 	prefix       = flag.String("prefix", "", "s3 prefix")
-	gzip         = flag.Bool("gzip_flag", true, "set to false if the files on S3 aren't gzipped")
-	rulePort     = flag.String("rule_port", "8080", "port to listen for new rules on")
+	gzip         = flag.Bool("gzip-flag", true, "set to false if the files on S3 aren't gzipped")
+	rulePort     = flag.String("rule-port", "8080", "port to listen for new rules on")
 	name         = flag.String("name", "poll-s3", "name of block")
 )
 
