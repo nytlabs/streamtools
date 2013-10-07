c945eccd7a8e3620ff733a54f059f22d982f62f0
Nik Hanselmann <nikhan@gmail.com>
2013-10-07 20:58:14 +0000

    go fmt

--- blocks/to_websocket/main.go
+++ blocks/to_websocket/main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-    "flag"
-    "github.com/nytlabs/streamtools/streamtools"
-    "github.com/bitly/go-simplejson"
+	"flag"
+	"github.com/bitly/go-simplejson"
+	"github.com/nytlabs/streamtools/streamtools"
 )
 
 var (
-    topic = flag.String("topic", "", "topic to read from")
-    name  = flag.String("name", "to-websocket", "name of block")
-    port = flag.String("port", "8888","port to serve websocket")
-    handle = flag.String("handle", "/ws","handle to serve websocket on eg: /ws")
+	topic  = flag.String("topic", "", "topic to read from")
+	name   = flag.String("name", "to-websocket", "name of block")
+	port   = flag.String("port", "8888", "port to serve websocket")
+	handle = flag.String("handle", "/ws", "handle to serve websocket on eg: /ws")
 )
 
 func main() {
-    flag.Parse()
-    block := streamtools.NewInBlock(streamtools.ToWebSocket, *name)
+	flag.Parse()
+	block := streamtools.NewInBlock(streamtools.ToWebSocket, *name)
 
-    rule, _ := simplejson.NewJson([]byte("{}"))
-    rule.Set("port",*port)
-    rule.Set("handle",*handle)
-    block.RuleChan <- rule
+	rule, _ := simplejson.NewJson([]byte("{}"))
+	rule.Set("port", *port)
+	rule.Set("handle", *handle)
+	block.RuleChan <- rule
 
-    block.Run(*topic, "8080")
+	block.Run(*topic, "8080")
 }
--- streamtools/to_websocket.go
+++ streamtools/to_websocket.go
@@ -2,117 +2,117 @@
 package streamtools
 
 import (
-    "github.com/bitly/go-simplejson"
-    "log"
-    "code.google.com/p/go.net/websocket"
-    "net/http"
+	"code.google.com/p/go.net/websocket"
+	"github.com/bitly/go-simplejson"
+	"log"
+	"net/http"
 )
 
 type wsConnection struct {
-    ws *websocket.Conn
-    send chan string
+	ws   *websocket.Conn
+	send chan string
 }
- 
+
 func (c *wsConnection) reader(h *wsServer) {
-    for {
-        var message string
-        err := websocket.Message.Receive(c.ws, &message)
-        if err != nil {
-            break
-        }
-        h.broadcast <- message
-    }
-    c.ws.Close()
+	for {
+		var message string
+		err := websocket.Message.Receive(c.ws, &message)
+		if err != nil {
+			break
+		}
+		h.broadcast <- message
+	}
+	c.ws.Close()
 }
- 
+
 func (c *wsConnection) writer(h *wsServer) {
-    for message := range c.send {
-        err := websocket.Message.Send(c.ws, message)
-        if err != nil {
-            break
-        }
-    }
-    c.ws.Close()
+	for message := range c.send {
+		err := websocket.Message.Send(c.ws, message)
+		if err != nil {
+			break
+		}
+	}
+	c.ws.Close()
 }
 
 type wsServer struct {
-    connections map[*wsConnection]bool
-    broadcast chan string
-    register chan *wsConnection
-    unregister chan *wsConnection
+	connections map[*wsConnection]bool
+	broadcast   chan string
+	register    chan *wsConnection
+	unregister  chan *wsConnection
 }
 
 func (h *wsServer) run() {
-    for {
-        select {
-        case c := <-h.register:
-            h.connections[c] = true
-        case c := <-h.unregister:
-            delete(h.connections, c)
-            close(c.send)
-        case m := <-h.broadcast:
-            for c := range h.connections {
-                select {
-                case c.send <- m:
-                default:
-                    delete(h.connections, c)
-                    close(c.send)
-                    go c.ws.Close()
-                }
-            }
-        }
-    }
+	for {
+		select {
+		case c := <-h.register:
+			h.connections[c] = true
+		case c := <-h.unregister:
+			delete(h.connections, c)
+			close(c.send)
+		case m := <-h.broadcast:
+			for c := range h.connections {
+				select {
+				case c.send <- m:
+				default:
+					delete(h.connections, c)
+					close(c.send)
+					go c.ws.Close()
+				}
+			}
+		}
+	}
 }
 
-// ToWebSocket emits all messages from a topic into a base64 encoded websocket handle 
+// ToWebSocket emits all messages from a topic into a base64 encoded websocket handle
 func ToWebSocket(inChan chan *simplejson.Json, RuleChan chan *simplejson.Json) {
-    rules := <- RuleChan
-    wsPort, err := rules.Get("port").String()
-    wsHandle, err := rules.Get("handle").String()
+	rules := <-RuleChan
+	wsPort, err := rules.Get("port").String()
+	wsHandle, err := rules.Get("handle").String()
 
-    var wsConfig *websocket.Config
-    var h = wsServer{
-        broadcast:   make(chan string),
-        register:    make(chan *wsConnection),
-        unregister:  make(chan *wsConnection),
-        connections: make(map[*wsConnection]bool),
-    }
-    
-    wsConfig, err = websocket.NewConfig("ws://localhost:" + wsPort + "/", "http://localhost:" + wsPort + "/");
-    if err != nil {
-        log.Fatalf(err.Error())
-    }
+	var wsConfig *websocket.Config
+	var h = wsServer{
+		broadcast:   make(chan string),
+		register:    make(chan *wsConnection),
+		unregister:  make(chan *wsConnection),
+		connections: make(map[*wsConnection]bool),
+	}
 
-    wsConfig.Protocol = []string{"base64"}
+	wsConfig, err = websocket.NewConfig("ws://localhost:"+wsPort+"/", "http://localhost:"+wsPort+"/")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
 
-    http.Handle(wsHandle, websocket.Server{
-        Handler: func(ws *websocket.Conn){
-            c := &wsConnection{send: make(chan string), ws: ws}
-            h.register <- c
-            defer func() { h.unregister <- c }()
-            go c.writer(&h)
-            c.reader(&h)
-        }, 
-        Config: *wsConfig,
-    })
+	wsConfig.Protocol = []string{"base64"}
 
-    go h.run()
+	http.Handle(wsHandle, websocket.Server{
+		Handler: func(ws *websocket.Conn) {
+			c := &wsConnection{send: make(chan string), ws: ws}
+			h.register <- c
+			defer func() { h.unregister <- c }()
+			go c.writer(&h)
+			c.reader(&h)
+		},
+		Config: *wsConfig,
+	})
 
-    go func(){
-        if err := http.ListenAndServe("localhost:" + wsPort, nil); err != nil {
-            log.Fatal("ListenAndServe:", err)
-        }
-    }()
+	go h.run()
 
-    for {
-        select {
-        case <-RuleChan:
-        case msg := <-inChan:
-            m, err := msg.Encode()
-            if err != nil{
-                log.Println("could not encode JSON")
-            }
-            h.broadcast <- string( m )
-        }
-    }
+	go func() {
+		if err := http.ListenAndServe("localhost:"+wsPort, nil); err != nil {
+			log.Fatal("ListenAndServe:", err)
+		}
+	}()
+
+	for {
+		select {
+		case <-RuleChan:
+		case msg := <-inChan:
+			m, err := msg.Encode()
+			if err != nil {
+				log.Println("could not encode JSON")
+			}
+			h.broadcast <- string(m)
+		}
+	}
 }
