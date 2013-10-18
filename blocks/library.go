82845b74c36b04291c0d7c46f2d4025d9ed7dcca
Mike Dewar <mikedewar@gmail.com>
2013-10-18 21:23:43 +0000

    moved library over

---
+++ blocks/library.go
@@ -0,0 +1,36 @@
+package blocks
+
+import (
+	"log"
+)
+
+var (
+	Library     BlockLibrary
+	libraryBlob string
+)
+
+// A block library is a collection of possible block templates
+type BlockLibrary map[string]Block
+
+func BuildLibrary() {
+	log.Println("building block library")
+	Library = make(map[string]Block)
+
+	// BLOCKS
+
+	Library["ticker"] = Block{
+		Template: &BlockTemplate{
+			BlockType:  "ticker",
+			RouteNames: []string{},
+		},
+		Routine: Ticker,
+	}
+
+	Library["tolog"] = Block{
+		Template: &BlockTemplate{
+			BlockType:  "tolog",
+			RouteNames: []string{},
+		},
+		Routine: ToLog,
+	}
+}
