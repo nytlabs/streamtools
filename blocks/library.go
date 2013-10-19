1eafa4a67d9464ba9e5e1876c63f4cfd0366e132
Nik Hanselmann <nikhan@gmail.com>
2013-10-19 00:20:12 +0000

    only one definition for blocktype now

--- blocks/library.go
+++ blocks/library.go
@@ -12,33 +12,23 @@ var (
 // A block library is a collection of possible block templates
 type BlockLibrary map[string]Block
 
+// TODO: library should probably become a struct at some point...
+func AddBlock(name string, routeNames []string, routine BlockRoutine){
+	Library[name] = Block{
+		Template: &BlockTemplate{
+			BlockType: name,
+			RouteNames: routeNames,
+		},
+		Routine: routine,
+	}
+}
+
 func BuildLibrary() {
 	log.Println("building block library")
 	Library = make(map[string]Block)
 
 	// BLOCKS
-
-	Library["ticker"] = Block{
-		Template: &BlockTemplate{
-			BlockType:  "ticker",
-			RouteNames: []string{},
-		},
-		Routine: Ticker,
-	}
-
-	Library["connection"] = Block{
-		Template: &BlockTemplate{
-			BlockType:  "connection",
-			RouteNames: []string{"last_seen"},
-		},
-		Routine: Connection,
-	}
-
-	Library["tolog"] = Block{
-		Template: &BlockTemplate{
-			BlockType:  "tolog",
-			RouteNames: []string{},
-		},
-		Routine: ToLog,
-	}
+	AddBlock("ticker", []string{}, Ticker)
+	AddBlock("connection", []string{"last_seen"}, Connection)
+	AddBlock("tolog", []string{}, ToLog)
 }
