8cd35b4c4a5199a91f9796345bd0e1c5e66bf938
Mike Dewar <mikedewar@gmail.com>
2013-10-19 19:03:44 -0400

    random and count

---
+++ blocks/count.go
@@ -0,0 +1,58 @@
+package blocks
+
+import (
+    "container/heap"
+    "github.com/bitly/go-simplejson"
+    "log"
+    "time"
+)
+
+func Count(b *Block) {
+    // block until we recieve a rule
+    params := <-b.Routes["params"]
+    windowSeconds, err := params.Msg.Get("window").Float64()
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+    window := time.Duration(windowSeconds) * time.Second
+    waitTimer := time.NewTimer(100 * time.Millisecond)
+
+    pq := &PriorityQueue{}
+    heap.Init(pq)
+
+    emptyByte := make([]byte, 0)
+
+    for {
+        select {
+        case query := <- b.Routes["count"]:
+            out, err := simplejson.NewJson([]byte("{}"))
+            if err != nil {
+                log.Fatal(err.Error())
+            }
+            out.Set("count", len(*pq))
+            query.ResponseChan <- out
+        case <- b.InChan:
+            queueMessage := &PQMessage{
+                val: &emptyByte,
+                t:   time.Now(),
+            }
+            if err != nil {
+                log.Fatal(err.Error())
+            }
+            heap.Push(pq, queueMessage)
+        case <-waitTimer.C:
+        }
+        for {
+            pqMsg, diff := pq.PeekAndShift(time.Now(), window)
+            if pqMsg == nil {
+                // either the queue is empty, or it's not time to emit
+                if diff == 0 {
+                    // then the queue is empty. Pause for 5 seconds before checking again
+                    diff = time.Duration(500) * time.Millisecond
+                }
+                waitTimer.Reset(diff)
+                break
+            }
+        }
+    }
+}
