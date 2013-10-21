aa0425bcb52805b0d0f31d371c66ef763f3e3e4d
Nik Hanselmann <nikhan@gmail.com>
2013-10-21 17:46:25 +0000

    gomt

--- blocks/mask.go
+++ blocks/mask.go
@@ -1,23 +1,23 @@
 package blocks
 
 import (
-      "github.com/bitly/go-simplejson"
-      "log"
+	"github.com/bitly/go-simplejson"
+	"log"
 )
 
 func maskJSON(mask map[string]interface{}, input map[string]interface{}) *simplejson.Json {
-    t, _ := simplejson.NewJson([]byte(`{}`))
+	t, _ := simplejson.NewJson([]byte(`{}`))
 
-    for k, _ := range mask {
-            switch input[k].(type) {
-            case map[string]interface{}:
-                    t.Set(k, maskJSON(mask[k].(map[string]interface{}), input[k].(map[string]interface{})))
-            default:
-                    t.Set(k, input[k])
-            }
-    }
+	for k, _ := range mask {
+		switch input[k].(type) {
+		case map[string]interface{}:
+			t.Set(k, maskJSON(mask[k].(map[string]interface{}), input[k].(map[string]interface{})))
+		default:
+			t.Set(k, input[k])
+		}
+	}
 
-    return t
+	return t
 }
 
 // Mask modifies a JSON stream with an additive key filter. Mask uses the JSON
@@ -32,27 +32,27 @@ func maskJSON(mask map[string]interface{}, input map[string]interface{}) *simple
 // The resulting object after the application of Mask would be:
 //        {"a":24, "b":{"d":[1,3,4]}, "x":{"y":5, "z":10}}
 func Mask(b *Block) {
-        type maskRule struct {
-            Mask interface{}
-        }
-        rule := &maskRule{}
+	type maskRule struct {
+		Mask interface{}
+	}
+	rule := &maskRule{}
 
-        unmarshal(<-b.Routes["set_rule"], &rule)
+	unmarshal(<-b.Routes["set_rule"], &rule)
 
-        for {
-                select {
-                case m := <-b.Routes["set_rule"]:
-                    unmarshal(m, &rule)
-                case r := <-b.Routes["get_rule"]:
-                    marshal(r, rule)
-                case msg := <-b.InChan:
-                    m, err := msg.Map()
-                    if err != nil{
-                        log.Println(err.Error())
-                    }
-                    broadcast(b.OutChans, maskJSON(rule.Mask.(map[string]interface{}), m))
-                case msg := <-b.AddChan:
-                    updateOutChans(msg, b)
-                }
-        }
+	for {
+		select {
+		case m := <-b.Routes["set_rule"]:
+			unmarshal(m, &rule)
+		case r := <-b.Routes["get_rule"]:
+			marshal(r, rule)
+		case msg := <-b.InChan:
+			m, err := msg.Map()
+			if err != nil {
+				log.Println(err.Error())
+			}
+			broadcast(b.OutChans, maskJSON(rule.Mask.(map[string]interface{}), m))
+		case msg := <-b.AddChan:
+			updateOutChans(msg, b)
+		}
+	}
 }
