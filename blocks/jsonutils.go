3501383d3053b2f1b659b3fac1474139e569b30b
Nik Hanselmann <nikhan@gmail.com>
2013-10-24 04:47:43 +0000

    adding jsonutil getkeyvalues

--- blocks/jsonutils.go
+++ blocks/jsonutils.go
@@ -9,6 +9,53 @@ func getPath(msg *simplejson.Json, path string) interface{} {
 	return msg.Get(path).Interface()
 }
 
+// getKeyValues returns values for all paths, including arrays
+// {"foo":"bar"} returns [bar] for string "foo"
+// {"foo":["bar","bar","bar"]} returns [bar, bar, bar] for string "foo"
+// {"foo":[{"type":"bar"},{"type":"baz"}]} returns [bar, baz] for string "foo.type"
+func getKeyValues(d interface{}, p string) []interface{} {
+	var values []interface{}
+	var key string
+	var rest string
+
+	keyIdx := strings.Index(p, ".")
+	
+	if keyIdx != -1 {
+		key = p[:keyIdx]
+		rest = p[keyIdx + 1:]
+	}
+
+	switch d := d.(type) {
+	case map[string]interface{}:
+		if len(rest) > 0 {
+			x := getKeyValues(d[key], rest)
+			for _, z := range x{
+				values = append(values, z)
+			}
+		} else {
+			if a, ok := (d[p]).([]interface{}); ok {
+				for _, elem := range a {
+					values = append(values, elem)
+				}
+			} else {
+				values = append(values, d[p])
+			}
+		}
+	case []interface{}:
+		for _, elem := range d {
+			if len(p) > 0 {
+				x := getKeyValues(elem, p)
+				for _, z := range x {
+					values = append(values, z)
+				}
+			}
+		}
+	default:
+	}
+
+	return values
+}
+
 func equals(value interface{}, comparator interface{}) bool {
 	switch value := value.(type) {
 	case int:
