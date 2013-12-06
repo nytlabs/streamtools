585b2ca5859924261c0040c697813890705d8095
Jacqui Maher <jacqui.maher@nytimes.com>
2013-12-06 11:25:56 -0500

    timeseries blocks now accommodate lag

--- examples/test_timeseries.sh
+++ examples/test_timeseries.sh
@@ -1,4 +1,6 @@
-./create random
-./create timeseries
-curl localhost:7070/blocks/2/set_rule --data '{"NumSamples":10, "Key":"random_float"}'
-./connect 1 2
+curl "localhost:7070/create?blockType=random&id=r"
+curl "localhost:7070/create?blockType=timeseries&id=t"
+curl localhost:7070/blocks/t/set_rule --data '{"NumSamples":10, "Key":"random_float"}'
+curl "localhost:7070/connect?from=r&to=t"
+
+curl localhost:7070/blocks/t/set_rule --data '{"NumSamples":10, "Key":"random_float", "Lag": 1800}'
