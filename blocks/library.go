31d093233cccf219f3f61b6c54a35eb960a39e32
Nik Hanselmann <nikhan@gmail.com>
2013-12-12 22:09:05 +0000

    RULES AS DATA

--- blocks/library.go
+++ blocks/library.go
@@ -84,11 +84,6 @@ func BuildLibrary() {
 			Routine:    FromNSQ,
 		},
 		&BlockTemplate{
-			BlockType:  "pollS3",
-			RouteNames: []string{"set_rule", "poll_now"},
-			Routine:    PollS3,
-		},
-		&BlockTemplate{
 			BlockType:  "tofile",
 			RouteNames: []string{"set_rule", "get_rule"},
 			Routine:    ToFile,
