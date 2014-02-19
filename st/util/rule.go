8ec5646d30c188395b522f50ee2e2cd6c5878131
Jacqui Maher <jacqui.maher@nytimes.com>
2014-02-19 18:28:26 -0500

    we now have some util funcs

---
+++ st/util/rule.go
@@ -0,0 +1,63 @@
+package util
+
+import "errors"
+
+func ParseBool(rule map[string]interface{}, key string) (bool, error) {
+	var val bool
+	var ok bool
+
+	ruleI, ok := rule[key]
+	if !ok {
+		return val, errors.New("Key was not in rule")
+	}
+	val, ok = ruleI.(bool)
+	if !ok {
+		return val, errors.New("Key's value was not a bool")
+	}
+	return val, nil
+}
+
+func ParseString(rule map[string]interface{}, key string) (string, error) {
+	var val string
+	var ok bool
+
+	ruleI, ok := rule[key]
+	if !ok {
+		return val, errors.New("Key was not in rule")
+	}
+	val, ok = ruleI.(string)
+	if !ok {
+		return val, errors.New("Key was not a string")
+	}
+	return val, nil
+}
+
+func ParseFloat(rule map[string]interface{}, key string) (float64, error) {
+	var val float64
+	var ok bool
+
+	ruleI, ok := rule[key]
+	if !ok {
+		return val, errors.New("Key was not in rule")
+	}
+	val, ok = ruleI.(float64)
+	if !ok {
+		return val, errors.New("Key was not a float64")
+	}
+	return val, nil
+}
+
+func ParseInt(rule map[string]interface{}, key string) (int, error) {
+	var val int
+	var ok bool
+
+	ruleI, ok := rule[key]
+	if !ok {
+		return val, errors.New("Key was not in rule")
+	}
+	val, ok = ruleI.(int)
+	if !ok {
+		return val, errors.New("Key was not an int!")
+	}
+	return val, nil
+}
