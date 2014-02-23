6b84499f6080cb438257da5329d0a4d434d17f8e
Mike Dewar <mikedewar@gmail.com>
2014-02-23 15:15:04 +0000

    initial commit of histogram block. I've not really started it yet thought

---
+++ st/library/histogram.go
@@ -0,0 +1,52 @@
+package library
+
+import (
+	"github.com/nytlabs/streamtools/st/blocks" // blocks
+)
+
+// specify those channels we're going to use to communicate with streamtools
+type Histogram struct {
+	blocks.Block
+	queryrule chan chan interface{}
+	inrule    chan interface{}
+	inpoll    chan interface{}
+	in        chan interface{}
+	out       chan interface{}
+	quit      chan interface{}
+}
+
+// we need to build a simple factory so that streamtools can make new blocks of this kind
+func NewHistogram() blocks.BlockInterface {
+	return &Histogram{}
+}
+
+// Setup is called once before running the block. We build up the channels and specify what kind of block this is.
+func (b *Histogram) Setup() {
+	b.Kind = "Histogram"
+	b.in = b.InRoute("in")
+	b.inrule = b.InRoute("rule")
+	b.queryrule = b.QueryRoute("rule")
+	b.inpoll = b.InRoute("poll")
+	b.quit = b.InRoute("quit")
+	b.out = b.Broadcast()
+}
+
+// Run is the block's main loop. Here we listen on the different channels we set up.
+func (b *Histogram) Run() {
+	for {
+		select {
+		case ruleI := <-b.inrule:
+			// set a parameter of the block
+			_, _ = ruleI.(map[string]interface{})
+		case <-b.quit:
+			// quit the block
+			return
+		case _ = <-b.in:
+			// deal with inbound data
+		case <-b.inpoll:
+			// deal with a poll request
+		case _ = <-b.queryrule:
+			// deal with a query request
+		}
+	}
+}
