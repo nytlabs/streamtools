b5474c046f3766cd213dd9e21a7b8fb0777c5281
Jacqui Maher <jacqui.maher@nytimes.com>
2014-02-25 11:39:26 -0500

    Bugfix: b.Quit()

--- st/library/timeseries.go
+++ st/library/timeseries.go
@@ -40,7 +40,7 @@ func (b *Timeseries) Setup() {
 	b.inrule = b.InRoute("rule")
 	b.queryrule = b.QueryRoute("rule")
 	b.inpoll = b.InRoute("poll")
-	b.quit = b.InRoute("quit")
+	b.quit = b.Quit()
 	b.out = b.Broadcast()
 }
 
