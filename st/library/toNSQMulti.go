51f91191d7b32ff012e881dbf33daf3e522681a4
Nik Hanselmann <nikhan@gmail.com>
2014-03-06 03:43:04 +0000

    adding multi publish for nsq

--- st/library/toNSQMulti.go
+++ st/library/toNSQMulti.go
@@ -5,10 +5,11 @@ import (
 	"github.com/bitly/go-nsq"
 	"github.com/nytlabs/streamtools/st/blocks" // blocks
 	"github.com/nytlabs/streamtools/st/util"
+	"time"
 )
 
 // specify those channels we're going to use to communicate with streamtools
-type ToNSQ struct {
+type ToNSQMulti struct {
 	blocks.Block
 	queryrule    chan chan interface{}
 	inrule       chan interface{}
@@ -20,12 +21,12 @@ type ToNSQ struct {
 }
 
 // a bit of boilerplate for streamtools
-func NewToNSQ() blocks.BlockInterface {
-	return &ToNSQ{}
+func NewToNSQMulti() blocks.BlockInterface {
+	return &ToNSQMulti{}
 }
 
-func (b *ToNSQ) Setup() {
-	b.Kind = "ToNSQ"
+func (b *ToNSQMulti) Setup() {
+	b.Kind = "ToNSQMulti"
 	b.in = b.InRoute("in")
 	b.inrule = b.InRoute("rule")
 	b.queryrule = b.QueryRoute("rule")
@@ -34,11 +35,25 @@ func (b *ToNSQ) Setup() {
 }
 
 // connects to an NSQ topic and emits each message into streamtools.
-func (b *ToNSQ) Run() {
+func (b *ToNSQMulti) Run() {
 	var writer *nsq.Writer
+	var batch [][]byte
+	interval := time.Duration(1 * time.Second)
+	maxBatch := 100
 
+	dump := time.NewTicker(interval)
 	for {
 		select {
+		case <-dump.C:
+			if writer == nil {
+				break
+			}
+			_, _, err := writer.MultiPublish(b.topic, batch)
+			if err != nil {
+				b.Error(err.Error())
+			}
+
+			batch = nil
 		case ruleI := <-b.inrule:
 			//rule := ruleI.(map[string]interface{})
 
@@ -54,34 +69,72 @@ func (b *ToNSQ) Run() {
 				break
 			}
 
+			intervalS, err := util.ParseString(ruleI, "Interval")
+			if err != nil {
+				b.Error("bad input")
+				break
+			}
+
+			dur, err := time.ParseDuration(intervalS)
+			if err != nil {
+				b.Error(err)
+				break
+			}
+
+			if dur <= 0 {
+				b.Error("interval must be positive")
+				break
+			}
+
+			batchSize, err := util.ParseFloat(ruleI, "MaxBatch")
+			if err != nil {
+				b.Error("error parsing batch size")
+				break
+			}
+
 			if writer != nil {
 				writer.Stop()
 			}
 
-			writer = nsq.NewWriter(nsqdTCPAddrs)
+			maxBatch = int(batchSize)
+			interval = dur
 
+			dump.Stop()
+			dump = time.NewTicker(interval)
+			writer = nsq.NewWriter(nsqdTCPAddrs)
 			b.topic = topic
 			b.nsqdTCPAddrs = nsqdTCPAddrs
-
 		case msg := <-b.in:
-			msgStr, err := json.Marshal(msg)
-			if err != nil {
-				b.Error(err)
-			}
-			_, _, err = writer.Publish(b.topic, []byte(msgStr))
-			if err != nil {
-				b.Error(err)
+			if writer == nil {
+				break
 			}
 
+			msgByte, err := json.Marshal(msg)
+			if err != nil {
+				b.Error(err)
+			}
+			batch = append(batch, msgByte)
+
+			if len(batch) > maxBatch {
+				_, _, err := writer.MultiPublish(b.topic, batch)
+				if err != nil {
+					b.Error(err.Error())
+					break
+				}
+				batch = nil
+			}
 		case <-b.quit:
 			if writer != nil {
 				writer.Stop()
 			}
+			dump.Stop()
 			return
 		case c := <-b.queryrule:
 			c <- map[string]interface{}{
 				"Topic":        b.topic,
 				"NsqdTCPAddrs": b.nsqdTCPAddrs,
+				"MaxBatch":	    maxBatch,
+				"Interval":		interval.String(),
 			}
 		}
 	}
