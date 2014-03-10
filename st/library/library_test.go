be8a5f33d46f2cbd261f9e4cd05c9286dae3e166
Mike Dewar <mikedewar@gmail.com>
2014-03-10 21:21:03 +0000

    set test

--- st/library/library_test.go
+++ st/library/library_test.go
@@ -804,3 +804,38 @@ func (s *StreamSuite) TestPack(c *C) {
 		}
 	}
 }
+
+func (s *StreamSuite) TestSet(c *C) {
+	loghub.Start()
+	log.Println("testing set")
+	b, ch := newBlock("testing set", "set")
+	go blocks.BlockRoutine(b)
+	outChan := make(chan *blocks.Msg)
+	ch.AddChan <- &blocks.AddChanMsg{
+		Route:   "out",
+		Channel: outChan,
+	}
+	time.AfterFunc(time.Duration(5)*time.Second, func() {
+		ch.QuitChan <- true
+	})
+	ruleMsg := map[string]interface{}{"Path": ".a"}
+	rule := &blocks.Msg{Msg: ruleMsg, Route: "rule"}
+	ch.InChan <- rule
+	/*
+		m := map[string]string{"b": "test"}
+		arr := []interface{}{m}
+		inMsg := map[string]interface{}{"a": arr}
+		ch.InChan <- &blocks.Msg{Msg: inMsg, Route: "in"}
+	*/
+	for {
+		select {
+		case err := <-ch.ErrChan:
+			if err != nil {
+				c.Errorf(err.Error())
+			} else {
+				return
+			}
+		case <-outChan:
+		}
+	}
+}
