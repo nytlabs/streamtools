aac40cb2a7d7cd763b98308e05683bae430740d5
Mike Dewar <mikedewar@gmail.com>
2014-03-19 16:19:07 +0000

    added fft query to timeseries

--- st/library/timeseries.go
+++ st/library/timeseries.go
@@ -2,6 +2,7 @@ package library
 
 import (
 	"errors"
+	"github.com/mjibson/go-dsp/fft"            // fft
 	"github.com/nytlabs/gojee"                 // jee
 	"github.com/nytlabs/streamtools/st/blocks" // blocks
 	"github.com/nytlabs/streamtools/st/util"   // util
@@ -13,6 +14,7 @@ type Timeseries struct {
 	blocks.Block
 	queryrule  chan chan interface{}
 	querystate chan chan interface{}
+	queryfft   chan chan interface{}
 	inrule     chan interface{}
 	inpoll     chan interface{}
 	in         chan interface{}
@@ -41,6 +43,7 @@ func (b *Timeseries) Setup() {
 	b.inrule = b.InRoute("rule")
 	b.queryrule = b.QueryRoute("rule")
 	b.querystate = b.QueryRoute("timeseries")
+	b.queryfft = b.QueryRoute("fft")
 	b.inpoll = b.InRoute("poll")
 	b.quit = b.Quit()
 	b.out = b.Broadcast()
@@ -56,6 +59,9 @@ func (b *Timeseries) Run() {
 	//var lag time.Duration
 	var data tsData
 	var numSamples float64
+
+	// defaults
+	numSamples = 1
 
 	for {
 		select {
@@ -140,6 +146,23 @@ func (b *Timeseries) Run() {
 		case respChan := <-b.querystate:
 			out := map[string]interface{}{
 				"timeseries": data,
+			}
+			respChan <- out
+		case respChan := <-b.queryfft:
+			x := make([]float64, len(data.Values))
+			for i, d := range data.Values {
+				x[i] = d.Value
+			}
+			X := fft.FFTReal(x)
+
+			Xout := make([][]float64, len(X))
+			for i, Xi := range X {
+				Xout[i] = make([]float64, 2)
+				Xout[i][0] = real(Xi)
+				Xout[i][1] = imag(Xi)
+			}
+			out := map[string]interface{}{
+				"fft": Xout,
 			}
 			respChan <- out
 		case <-b.inpoll:
