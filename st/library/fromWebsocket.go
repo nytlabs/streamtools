004bd9f37597b0760a4adc3f4c9ee95f43e21002
Jacqui Maher <jacqui.maher@nytimes.com>
2014-03-19 15:07:23 -0400

    websocket block now also passes on data that can't be json.Unmarshalled

--- st/library/fromWebsocket.go
+++ st/library/fromWebsocket.go
@@ -1,107 +1,99 @@
 package library
 
 import (
-    "github.com/nytlabs/streamtools/st/blocks" // blocks
-    "github.com/gorilla/websocket"
-    "encoding/json"
-    "time"
-    "net/http"
+	"encoding/json"
+	"github.com/gorilla/websocket"
+	"github.com/nytlabs/streamtools/st/blocks" // blocks
+	"github.com/nytlabs/streamtools/st/util"
+	"net/http"
+	"time"
 )
 
 // specify those channels we're going to use to communicate with streamtools
 type FromWebsocket struct {
-    blocks.Block
-    queryrule chan chan interface{}
-    inrule    chan interface{}
-    inpoll    chan interface{}
-    in        chan interface{}
-    out       chan interface{}
-    quit      chan interface{}
+	blocks.Block
+	queryrule chan chan interface{}
+	inrule    chan interface{}
+	inpoll    chan interface{}
+	in        chan interface{}
+	out       chan interface{}
+	quit      chan interface{}
 }
 
 // we need to build a simple factory so that streamtools can make new blocks of this kind
 func NewFromWebsocket() blocks.BlockInterface {
-    return &FromWebsocket{}
+	return &FromWebsocket{}
 }
 
 // Setup is called once before running the block. We build up the channels and specify what kind of block this is.
 func (b *FromWebsocket) Setup() {
-    b.Kind = "FromWebsocket"
-    b.inrule = b.InRoute("rule")
-    b.queryrule = b.QueryRoute("rule")
-    b.quit = b.Quit()
-    b.out = b.Broadcast()
+	b.Kind = "FromWebsocket"
+	b.inrule = b.InRoute("rule")
+	b.queryrule = b.QueryRoute("rule")
+	b.quit = b.Quit()
+	b.out = b.Broadcast()
 }
 
-func recv(ws *websocket.Conn, out chan interface{}){
-    for {
-        _, p, err := ws.ReadMessage()
-        if err != nil {
-           return
-        }
+func recv(ws *websocket.Conn, out chan interface{}) {
+	for {
+		_, p, err := ws.ReadMessage()
+		if err != nil {
+			return
+		}
 
-        var outMsg interface{}
-        err = json.Unmarshal(p, &outMsg)
-        if err != nil {
-            continue
-        }
-        out <- outMsg
-    }
+		var outMsg interface{}
+		err = json.Unmarshal(p, &outMsg)
+		// if the json parsing fails, store data unparsed as "data"
+		if err != nil {
+			outMsg = map[string]interface{}{
+				"data": p,
+			}
+		}
+		out <- outMsg
+	}
 }
 
 // Run is the block's main loop. Here we listen on the different channels we set up.
 func (b *FromWebsocket) Run() {
-    var ws *websocket.Conn
-    var URL string
-    var handshakeDialer = &websocket.Dialer{
-        Subprotocols:    []string{"p1", "p2"},
-    }
-    listenWS := make(chan interface{})
-    wsHeader := http.Header{"Origin": {"http://localhost/"}}
+	var ws *websocket.Conn
+	var url string
+	var handshakeDialer = &websocket.Dialer{
+		Subprotocols: []string{"p1", "p2"},
+	}
+	listenWS := make(chan interface{})
+	wsHeader := http.Header{"Origin": {"http://localhost/"}}
 
-    for {
-        select {
-        case ruleI := <-b.inrule:
-            var err error
-            // set a parameter of the block
-            r, ok := ruleI.(map[string]interface{})
-            if !ok {
-                b.Error("bad rule")
-                break
-            }
+	for {
+		select {
+		case ruleI := <-b.inrule:
+			var err error
+			// set a parameter of the block
+			url, err := util.ParseString(ruleI, "url")
+			if err != nil {
+				b.Error(err)
+				continue
+			}
+			if ws != nil {
+				ws.Close()
+			}
 
-            url, ok := r["url"]
-            if !ok {
-                b.Error("no url specified")
-                break
-            }
-            surl, ok := url.(string)
-            if !ok {
-                b.Error("error reading url")
-                break
-            }
-            if ws != nil {
-                ws.Close()
-            }
+			ws, _, err = handshakeDialer.Dial(url, wsHeader)
+			if err != nil {
+				b.Error("could not connect to url")
+				break
+			}
+			ws.SetReadDeadline(time.Time{})
+			go recv(ws, listenWS)
 
-            ws, _, err = handshakeDialer.Dial(surl, wsHeader)          
-            if err != nil {
-                b.Error("could not connect to url")
-                break
-            }
-            ws.SetReadDeadline(time.Time{})  
-            go recv(ws, listenWS)
-
-            URL = surl
-        case <-b.quit:
-            // quit the block
-            return
-        case o := <-b.queryrule:
-            o <- map[string]interface{}{
-                "url": URL,
-            }
-        case in := <- listenWS:
-            b.out <- in
-        }
-    }
+		case <-b.quit:
+			// quit the block
+			return
+		case o := <-b.queryrule:
+			o <- map[string]interface{}{
+				"url": url,
+			}
+		case in := <-listenWS:
+			b.out <- in
+		}
+	}
 }
