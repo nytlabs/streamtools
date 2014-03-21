7f72de591f6df190da914cb7ad4d658e3960d4c1
Deep Kapadia <deepkapadia@gmail.com>
2014-03-21 11:09:20 -0400

    fixed tabs...gaah

--- st/library/toBeanstalkd.go
+++ st/library/toBeanstalkd.go
@@ -1,37 +1,37 @@
 package library
 
 import (
-	"encoding/json"
-	"github.com/nytlabs/streamtools/st/blocks" // blocks
-	"github.com/nytlabs/streamtools/st/util"
+    "encoding/json"
+    "github.com/nytlabs/streamtools/st/blocks" // blocks
+    "github.com/nytlabs/streamtools/st/util"
     "github.com/nutrun/lentil"
     "errors"
 )
 
 // specify those channels we're going to use to communicate with streamtools
 type ToBeanstalkd struct {
-	blocks.Block
-	host      string
+    blocks.Block
+    host      string
     tube      string
     ttr       int
-	queryrule chan chan interface{}
-	inrule    chan interface{}
-	in        chan interface{}
-	quit      chan interface{}
+    queryrule chan chan interface{}
+    inrule    chan interface{}
+    in        chan interface{}
+    quit      chan interface{}
 }
 
 // we need to build a simple factory so that streamtools can make new blocks of this kind
 func NewToBeanstalkd() blocks.BlockInterface {
-	return &ToBeanstalkd{}
+    return &ToBeanstalkd{}
 }
 
 // Setup is called once before running the block. We build up the channels and specify what kind of block this is.
 func (b *ToBeanstalkd) Setup() {
-	b.Kind = "ToBeanstalkd"
-	b.in = b.InRoute("in")
-	b.inrule = b.InRoute("rule")
-	b.queryrule = b.QueryRoute("rule")
-	b.quit = b.Quit()
+    b.Kind = "ToBeanstalkd"
+    b.in = b.InRoute("in")
+    b.inrule = b.InRoute("rule")
+    b.queryrule = b.QueryRoute("rule")
+    b.quit = b.Quit()
 }
 
 // Run is the block's main loop. Here we listen on the different channels we set up.
@@ -39,11 +39,11 @@ func (b *ToBeanstalkd) Run() {
     var conn *lentil.Beanstalkd
     var tube = "default"
     var ttr = 0
-	for {
-		select {
-		case msgI := <-b.inrule:
-			// set hostname for beanstalkd server
-			host, err := util.ParseString(msgI, "Host")
+    for {
+        select {
+        case msgI := <-b.inrule:
+            // set hostname for beanstalkd server
+            host, err := util.ParseString(msgI, "Host")
             if err != nil {
                 b.Error(err.Error())
                 continue
@@ -60,7 +60,7 @@ func (b *ToBeanstalkd) Run() {
             }
             b.ttr = ttr
             // create beanstalkd connection
-			conn, err = lentil.Dial(b.host)
+            conn, err = lentil.Dial(b.host)
             if err != nil {
                 // swallowing a panic from lentil here - streamtools must not die
                 b.Error(errors.New("Could not initiate connection with beanstalkd server"))
@@ -68,18 +68,18 @@ func (b *ToBeanstalkd) Run() {
             }
             // use the specified tube
             conn.Use(b.tube)
-		case <-b.quit:
-			// close connection to beanstalkd and quit
-			if conn != nil {
-				conn.Quit()
-			}
-			return
-		case msg := <-b.in:
-			// deal with inbound data
-			msgStr, err := json.Marshal(msg)
-			if err != nil {
-				b.Error(err)
-			}
+        case <-b.quit:
+            // close connection to beanstalkd and quit
+            if conn != nil {
+                conn.Quit()
+            }
+            return
+        case msg := <-b.in:
+            // deal with inbound data
+            msgStr, err := json.Marshal(msg)
+            if err != nil {
+                b.Error(err)
+            }
             if conn != nil {
                 _, err :=  conn.Put(0, 0 , b.ttr, msgStr)
                 if err != nil {
@@ -88,13 +88,13 @@ func (b *ToBeanstalkd) Run() {
             } else {
                 b.Error(errors.New("Beanstalkd connection not initated or lost. Please check your beanstalkd server or block settings."))
             }
-		case respChan := <-b.queryrule:
-			// deal with a query request
-			respChan <- map[string]interface{}{
-				"Host": b.host,
+        case respChan := <-b.queryrule:
+            // deal with a query request
+            respChan <- map[string]interface{}{
+                "Host": b.host,
                 "Tube": tube,
                 "TTR" : b.ttr,
-			}
-		}
-	}
+            }
+        }
+    }
 }
--- st/util/rule.go
+++ st/util/rule.go
@@ -1,79 +1,79 @@
 package util
 
 import (
-	"errors"
-	"github.com/nytlabs/gojee"
+    "errors"
+    "github.com/nytlabs/gojee"
 )
 
 func ParseBool(ruleI interface{}, key string) (bool, error) {
-	rule := ruleI.(map[string]interface{})
-	var val bool
-	var ok bool
+    rule := ruleI.(map[string]interface{})
+    var val bool
+    var ok bool
 
-	foundRule, ok := rule[key]
-	if !ok {
-		return val, errors.New("Key was not in rule")
-	}
-	val, ok = foundRule.(bool)
-	if !ok {
-		return val, errors.New("Key's value was not a bool")
-	}
-	return val, nil
+    foundRule, ok := rule[key]
+    if !ok {
+        return val, errors.New("Key was not in rule")
+    }
+    val, ok = foundRule.(bool)
+    if !ok {
+        return val, errors.New("Key's value was not a bool")
+    }
+    return val, nil
 }
 
 func ParseString(ruleI interface{}, key string) (string, error) {
-	rule := ruleI.(map[string]interface{})
-	var val string
-	var ok bool
+    rule := ruleI.(map[string]interface{})
+    var val string
+    var ok bool
 
-	foundRule, ok := rule[key]
-	if !ok {
-		return val, errors.New("Key was not in rule")
-	}
-	val, ok = foundRule.(string)
-	if !ok {
-		return val, errors.New("Key was not a string")
-	}
-	return val, nil
+    foundRule, ok := rule[key]
+    if !ok {
+        return val, errors.New("Key was not in rule")
+    }
+    val, ok = foundRule.(string)
+    if !ok {
+        return val, errors.New("Key was not a string")
+    }
+    return val, nil
 }
 
 func ParseFloat(ruleI interface{}, key string) (float64, error) {
-	rule := ruleI.(map[string]interface{})
-	var val float64
-	var ok bool
+    rule := ruleI.(map[string]interface{})
+    var val float64
+    var ok bool
 
-	foundRule, ok := rule[key]
-	if !ok {
-		return val, errors.New("Key was not in rule")
-	}
-	val, ok = foundRule.(float64)
-	if !ok {
-		return val, errors.New("Key was not a float64")
-	}
-	return val, nil
+    foundRule, ok := rule[key]
+    if !ok {
+        return val, errors.New("Key was not in rule")
+    }
+    val, ok = foundRule.(float64)
+    if !ok {
+        return val, errors.New("Key was not a float64")
+    }
+    return val, nil
 }
 
 func ParseInt(ruleI interface{}, key string) (int, error) {
-	rule := ruleI.(map[string]interface{})
-	var val int
+    rule := ruleI.(map[string]interface{})
+    var val int
     var ok bool
     var floatval float64
-	foundRule, ok := rule[key]
-	if !ok {
-		return val, errors.New("Key was not in rule")
-	}
+    foundRule, ok := rule[key]
+    if !ok {
+        return val, errors.New("Key was not in rule")
+    }
     floatval, ok = foundRule.(float64)
     if !ok {
         return val, errors.New("Key was not a number")
     }
-	val = int(floatval)
-	return val, nil
+    val = int(floatval)
+    return val, nil
 }
 
 func BuildTokenTree(path string) (tree *jee.TokenTree, err error) {
-	token, err := jee.Lexer(path)
-	if err != nil {
-		return nil, err
-	}
-	return jee.Parser(token)
+    token, err := jee.Lexer(path)
+    if err != nil {
+        return nil, err
+    }
+    return jee.Parser(token)
 }
