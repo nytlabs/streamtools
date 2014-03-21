e4279f627cd07b473639d0363af309a8c27ade65
Deep Kapadia <deepkapadia@gmail.com>
2014-03-21 11:14:52 -0400

    fixed tabs again using go fmt

--- st/library/toBeanstalkd.go
+++ st/library/toBeanstalkd.go
@@ -1,100 +1,100 @@
 package library
 
 import (
-    "encoding/json"
-    "github.com/nytlabs/streamtools/st/blocks" // blocks
-    "github.com/nytlabs/streamtools/st/util"
-    "github.com/nutrun/lentil"
-    "errors"
+	"encoding/json"
+	"errors"
+	"github.com/nutrun/lentil"
+	"github.com/nytlabs/streamtools/st/blocks" // blocks
+	"github.com/nytlabs/streamtools/st/util"
 )
 
 // specify those channels we're going to use to communicate with streamtools
 type ToBeanstalkd struct {
-    blocks.Block
-    host      string
-    tube      string
-    ttr       int
-    queryrule chan chan interface{}
-    inrule    chan interface{}
-    in        chan interface{}
-    quit      chan interface{}
+	blocks.Block
+	host      string
+	tube      string
+	ttr       int
+	queryrule chan chan interface{}
+	inrule    chan interface{}
+	in        chan interface{}
+	quit      chan interface{}
 }
 
 // we need to build a simple factory so that streamtools can make new blocks of this kind
 func NewToBeanstalkd() blocks.BlockInterface {
-    return &ToBeanstalkd{}
+	return &ToBeanstalkd{}
 }
 
 // Setup is called once before running the block. We build up the channels and specify what kind of block this is.
 func (b *ToBeanstalkd) Setup() {
-    b.Kind = "ToBeanstalkd"
-    b.in = b.InRoute("in")
-    b.inrule = b.InRoute("rule")
-    b.queryrule = b.QueryRoute("rule")
-    b.quit = b.Quit()
+	b.Kind = "ToBeanstalkd"
+	b.in = b.InRoute("in")
+	b.inrule = b.InRoute("rule")
+	b.queryrule = b.QueryRoute("rule")
+	b.quit = b.Quit()
 }
 
 // Run is the block's main loop. Here we listen on the different channels we set up.
 func (b *ToBeanstalkd) Run() {
-    var conn *lentil.Beanstalkd
-    var tube = "default"
-    var ttr = 0
-    for {
-        select {
-        case msgI := <-b.inrule:
-            // set hostname for beanstalkd server
-            host, err := util.ParseString(msgI, "Host")
-            if err != nil {
-                b.Error(err.Error())
-                continue
-            } 
-            b.host = host
-            // set tube name
-            tube, _ = util.ParseString(msgI, "Tube")
-            b.tube = tube
-            // set time to reserve
-            ttr, err = util.ParseInt(msgI, "TTR")
-            if err !=nil || ttr < 0 {
-                b.Error(errors.New("Error parsing TTR. Setting TTR to 0"))
-                ttr = 0
-            }
-            b.ttr = ttr
-            // create beanstalkd connection
-            conn, err = lentil.Dial(b.host)
-            if err != nil {
-                // swallowing a panic from lentil here - streamtools must not die
-                b.Error(errors.New("Could not initiate connection with beanstalkd server"))
-                continue
-            }
-            // use the specified tube
-            conn.Use(b.tube)
-        case <-b.quit:
-            // close connection to beanstalkd and quit
-            if conn != nil {
-                conn.Quit()
-            }
-            return
-        case msg := <-b.in:
-            // deal with inbound data
-            msgStr, err := json.Marshal(msg)
-            if err != nil {
-                b.Error(err)
-            }
-            if conn != nil {
-                _, err :=  conn.Put(0, 0 , b.ttr, msgStr)
-                if err != nil {
-                    b.Error(err.Error())
-                }
-            } else {
-                b.Error(errors.New("Beanstalkd connection not initated or lost. Please check your beanstalkd server or block settings."))
-            }
-        case respChan := <-b.queryrule:
-            // deal with a query request
-            respChan <- map[string]interface{}{
-                "Host": b.host,
-                "Tube": tube,
-                "TTR" : b.ttr,
-            }
-        }
-    }
+	var conn *lentil.Beanstalkd
+	var tube = "default"
+	var ttr = 0
+	for {
+		select {
+		case msgI := <-b.inrule:
+			// set hostname for beanstalkd server
+			host, err := util.ParseString(msgI, "Host")
+			if err != nil {
+				b.Error(err.Error())
+				continue
+			}
+			b.host = host
+			// set tube name
+			tube, _ = util.ParseString(msgI, "Tube")
+			b.tube = tube
+			// set time to reserve
+			ttr, err = util.ParseInt(msgI, "TTR")
+			if err != nil || ttr < 0 {
+				b.Error(errors.New("Error parsing TTR. Setting TTR to 0"))
+				ttr = 0
+			}
+			b.ttr = ttr
+			// create beanstalkd connection
+			conn, err = lentil.Dial(b.host)
+			if err != nil {
+				// swallowing a panic from lentil here - streamtools must not die
+				b.Error(errors.New("Could not initiate connection with beanstalkd server"))
+				continue
+			}
+			// use the specified tube
+			conn.Use(b.tube)
+		case <-b.quit:
+			// close connection to beanstalkd and quit
+			if conn != nil {
+				conn.Quit()
+			}
+			return
+		case msg := <-b.in:
+			// deal with inbound data
+			msgStr, err := json.Marshal(msg)
+			if err != nil {
+				b.Error(err)
+			}
+			if conn != nil {
+				_, err := conn.Put(0, 0, b.ttr, msgStr)
+				if err != nil {
+					b.Error(err.Error())
+				}
+			} else {
+				b.Error(errors.New("Beanstalkd connection not initated or lost. Please check your beanstalkd server or block settings."))
+			}
+		case respChan := <-b.queryrule:
+			// deal with a query request
+			respChan <- map[string]interface{}{
+				"Host": b.host,
+				"Tube": tube,
+				"TTR":  b.ttr,
+			}
+		}
+	}
 }
