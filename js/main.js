405d22bf6ae5c76e9ba3ce8a210ac433c100192f
Jane Friedhoff <friej715@newschool.edu>
2014-04-29 12:33:46 -0400

    choice of download options, new logo

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    // Display correct binary to download
+    var OSName="";
+    if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+    if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+    if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+    if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+
+    if (OSName == "Windows")    $(".download-windows").addClass('selected-OS');
+    if (OSName == "MacOS")      $(".download-mac").addClass('selected-OS');
+    if (OSName == "Linux")      $(".download-linux").addClass('selected-OS');
+
+    $(".download").not(".selected-OS").css('display', 'none');
+
+});
