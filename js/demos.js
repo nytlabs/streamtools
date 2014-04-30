d600e1b582ec4fbbd9d8c7d9921fc2f7736aea5e
Jacqui Maher <jacqui.maher@nytimes.com>
2014-04-30 14:14:45 -0400

    display screenshots when streamtools isnt running

--- js/demos.js
+++ js/demos.js
@@ -2,8 +2,8 @@ var checkForStreamtools = function() {
   $.ajax({
     dataType: "json",
     url: "http://localhost:7070/status",
-    success: function(data) {},
-    error: function() { $("#buttonPara").html("<div class='alert alert-warning'><p>Oops! Failed accessing <a target='_new' href='http://localhost:7070'>streamtools</a>. Need to download it? <a href='../#download'>Get the binary</a>, be setup in seconds.</p> <p><a href='" + window.location.href + "'>Reload</a> to see this demo in action once you're ready.</p></div>"); }
+    success: function(data) { $("#staticDemo").hide(); $("#getStarted").show(); },
+    error: function() { $("#getStarted").hide(); $("#staticDemo").show(); }
   });
 };
 
