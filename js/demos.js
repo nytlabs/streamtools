9a0c0fadcc287bd8999c50dcf7162bcaba5f9b5a
Jacqui Maher <jacqui.maher@nytimes.com>
2014-05-02 09:55:35 -0400

    Added warning about clearing streamtools when blocks exist

--- js/demos.js
+++ js/demos.js
@@ -2,7 +2,14 @@ var checkForStreamtools = function() {
   $.ajax({
     dataType: "json",
     url: "http://localhost:7070/status",
-    success: function(data) { $("#staticDemo").hide(); $("#getStarted").show(); },
+    success: function(data) {
+      if (data.Blocks && data.Blocks.length > 0) {
+        $("#leadPara").after("<p class='text-danger'><span class='glyphicon glyphicon-warning-sign'></span> It looks like you have blocks in your local streamtools. Starting this demo will clear out those blocks. Perhaps you should <a target='_new' href='http://localhost:7070/export'>export before continuing.</p>");
+      }
+
+      $("#staticDemo").hide();
+      $("#getStarted").show();
+    },
     error: function() { $("#getStarted").hide(); $("#staticDemo").show(); }
   });
 };
