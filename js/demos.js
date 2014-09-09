67787b2a127f93fbc2b6a1e48d87e1a3d787294d
Jacqui Maher <jacqui.maher@nytimes.com>
2014-09-09 11:09:14 -0400

    Switched to all quakes in the last hour, tidied up some js/css

--- js/demos.js
+++ js/demos.js
@@ -4,7 +4,7 @@ var checkForStreamtools = function() {
     url: "http://localhost:7070/status",
     success: function(data) {
       if (data.Blocks && data.Blocks.length > 0) {
-        $("#leadPara").after("<p class='text-danger'><span class='glyphicon glyphicon-warning-sign'></span> It looks like you have blocks in your local streamtools. Starting this demo will clear out those blocks. Perhaps you should <a target='_new' href='http://localhost:7070/export'>export before continuing.</p>");
+        $("#leadPara").after("<p class='text-danger' id='blocks-exist-warning'><span class='glyphicon glyphicon-warning-sign'></span> It looks like you have blocks in your local streamtools. Starting this demo will clear out those blocks. Perhaps you should <a target='_new' href='http://localhost:7070/export'>export before continuing.</p>");
       }
 
       $("#staticDemo").hide();
